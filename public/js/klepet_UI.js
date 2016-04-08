dd8c4664e0968f79e3e57a2a17de25ab6c338e11
Ivan Antešić <ia6382@student.uni-lj.si>
2016-04-08 20:26:36 +0000

    prototip slike

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -3,18 +3,44 @@ function divElementEnostavniTekst(sporocilo) {
   if (jeSmesko) {
     sporocilo = sporocilo.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace('&lt;img', '<img').replace('png\' /&gt;', 'png\' />');
     return $('<div style="font-weight: bold"></div>').html(sporocilo);
-  } else {
+  } 
+  else {
     return $('<div style="font-weight: bold;"></div>').text(sporocilo);
   }
+}
+
+function divElementSlika(sporocilo) {
+  return $('<div></div>').html(sporocilo);
 }
 
 function divElementHtmlTekst(sporocilo) {
   return $('<div></div>').html('<i>' + sporocilo + '</i>');
 }
 
+function izlusciHtmlImg(sporocilo){
+  //moras se splitatt med http(s) in jpg,.. in nato vse preveriti v zanki, smesko dela samo en
+  var img;
+  var reg = /(https|http)?:\/\/.*\.(?:png|jpg|gif)/;
+  img = sporocilo.match(reg);
+  
+  if(img != null){
+    var jeSmesko = img[0].indexOf('http://sandbox.lavbic.net/teaching/OIS/gradivo/') > -1;
+    img = img[0].replace(reg, function(url) {
+        return "<img src=\"" + url + "\" style=\"width:200px; position:relative; left: 20px;\">";
+    });
+  }
+  
+  if (jeSmesko) {
+    img = null;
+  }
+  console.log(img);
+  return img;
+}
+
 function procesirajVnosUporabnika(klepetApp, socket) {
   var sporocilo = $('#poslji-sporocilo').val();
   sporocilo = dodajSmeske(sporocilo);
+  var htmlImg = izlusciHtmlImg(sporocilo);
   var sistemskoSporocilo;
 
   if (sporocilo.charAt(0) == '/') {
@@ -26,6 +52,9 @@ function procesirajVnosUporabnika(klepetApp, socket) {
     sporocilo = filtirirajVulgarneBesede(sporocilo);
     klepetApp.posljiSporocilo(trenutniKanal, sporocilo);
     $('#sporocila').append(divElementEnostavniTekst(sporocilo));
+    if(htmlImg != null){
+       $('#sporocila').append(divElementSlika(htmlImg));
+    }
     $('#sporocila').scrollTop($('#sporocila').prop('scrollHeight'));
   }
 
@@ -75,7 +104,12 @@ $(document).ready(function() {
 
   socket.on('sporocilo', function (sporocilo) {
     var novElement = divElementEnostavniTekst(sporocilo.besedilo);
+    var htmlImg = izlusciHtmlImg(sporocilo.besedilo);
     $('#sporocila').append(novElement);
+    
+    if(htmlImg != null){
+       $('#sporocila').append(divElementSlika(htmlImg));
+    }
   });
   
   socket.on('kanali', function(kanali) {
@@ -131,3 +165,5 @@ function dodajSmeske(vhodnoBesedilo) {
   }
   return vhodnoBesedilo;
 }
+
+
